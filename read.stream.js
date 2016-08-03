04ce8444401e219a87ecc834864d25c75221bbb0
xiaoheng <625658476@qq.com>
2016-08-03 22:43:56 +0800

    read.stream.js
    read.stream 的简单操作

---
+++ read.stream.js
@@ -0,0 +1,11 @@
+var stream = require( "stream" );
+
+var Readable = stream.Readable;
+
+var rs = new Readable;
+
+rs.push( 'beep ' );
+rs.push( 'boop\n' );
+rs.push( null );
+
+rs.pipe(process.stdout);
